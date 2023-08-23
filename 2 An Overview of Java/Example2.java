a93f611eecfbf8d9ea0ae02faf70d8318e44d6f6
Naresh Sondkar <nareshsondkar30218@gmail.com>
2023-08-23 15:25:08 +0530

    Initial Commit

---
+++ 2 An Overview of Java/Example2.java
@@ -0,0 +1,13 @@
+public class Example2{
+    public static void main(String [] args){
+        int num;
+        num = 100;
+
+        System.out.println("The value of num is" + num);
+
+        num = num * 2;
+
+        System.out.print("The value of num * 2 is ");
+        System.out.println(num);
+    }
+}
