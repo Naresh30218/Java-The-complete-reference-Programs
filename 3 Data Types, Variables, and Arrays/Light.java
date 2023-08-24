0769fbe540a8bf150f9edc9ea2e627bb3f70a046
Naresh Sondkar <nareshsondkar30218@gmail.com>
2023-08-24 11:21:15 +0530

    Started Part 1 3rd Chapter

---
+++ 3 Data Types, Variables, and Arrays/Light.java
@@ -0,0 +1,20 @@
+public class Light{
+    public static void main(String [] args){
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+
+        lightspeed = 186000;
+
+        days = 1000;
+
+        seconds = days * 24 * 60 * 60;
+
+        distance = lightspeed * seconds;
+
+        System.out.println("In " + days);
+        System.out.print("day light will travel about ");
+        System.out.println(distance + " miles.");
+    }
+}
